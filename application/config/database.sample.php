12094333af2f1fbe83328e14483d7cda494ab7fc
Rob Gabaree <rob@rawb.net>
2010-04-13 12:40:41 -0400

    Updated sample database config to be cleaner

--- application/config/database.sample.php
+++ application/config/database.sample.php
@@ -1,53 +1,45 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
+<?php defined('SYSPATH') or die('No direct access allowed.');
 
 return array
 (
+
+	// Default instance (PDO)
 	'default' => array
 	(
-		'type'       => 'mysql',
-		'connection' => array(
-			/**
-			 * The following options are available for MySQL:
-			 *
-			 * string   hostname
-			 * string   username
-			 * string   password
-			 * boolean  persistent
-			 * string   database
-			 *
-			 * Ports and sockets may be appended to the hostname.
-			 */
-			'hostname'   => 'localhost',
-			'username'   => FALSE,
-			'password'   => FALSE,
-			'persistent' => FALSE,
-			'database'   => 'kohana',
+		'type'            => 'pdo',
+		'charset'         => 'utf8',
+		'table_prefix'    => '',
+		'profiling'       => TRUE,
+		'caching'         => FALSE,
+		'connection'      => array
+		(
+			'username'      => '',
+			'password'      => '',
+			'persistent'    => FALSE,
+			'dsn'           => 'mysql:host=localhost;dbname=jelly_auth_demo',
 		),
-		'table_prefix' => '',
-		'charset'      => 'utf8',
-		'caching'      => FALSE,
-		'profiling'    => TRUE,
-	),
-	'alternate' => array(
-		'type'       => 'pdo',
-		'connection' => array(
-			/**
-			 * The following options are available for PDO:
-			 *
-			 * string   dsn
-			 * string   username
-			 * string   password
-			 * boolean  persistent
-			 * string   identifier
-			 */
-			'dsn'        => 'mysql:host=localhost;dbname=kohana',
-			'username'   => 'root',
-			'password'   => 'r00tdb',
-			'persistent' => FALSE,
+	), // End default instance (PDO)
+
+	/*
+	
+	// Default instance (MySQL)
+	'default' => array
+	(
+		'type'            => 'mysql',
+		'charset'         => 'utf8',
+		'table_prefix'    => '',
+		'profiling'       => TRUE,
+		'caching'         => FALSE,
+		'connection'      => array
+		(
+			'hostname'      => 'localhost',
+			'username'      => '',
+			'password'      => '',
+			'persistent'    => FALSE,
+			'database'      => 'jelly_auth_demo',
 		),
-		'table_prefix' => '',
-		'charset'      => 'utf8',
-		'caching'      => FALSE,
-		'profiling'    => TRUE,
-	),
+	), // End default instance (MySQL)
+	
+	*/
+
 );
